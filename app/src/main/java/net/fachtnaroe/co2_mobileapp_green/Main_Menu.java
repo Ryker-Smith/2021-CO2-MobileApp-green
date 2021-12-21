f7eebfc7a23d7fa597dd5df16956d86dfa3a0e5c
SL21G <lshane461@outlook.ie>
2021-12-21 17:41:45 +0000

    updated the buttons and their functions

--- app/src/main/java/net/fachtnaroe/co2_mobileapp_green/Main_Menu.java
+++ app/src/main/java/net/fachtnaroe/co2_mobileapp_green/Main_Menu.java
@@ -41,7 +41,7 @@ public class Main_Menu extends Form implements HandlesEventDispatching{
     HorizontalArrangement HorizontalArrangementM9;
     HorizontalArrangement HorizontalArrangementM10;
 
-    Button buttonM1, buttonM5, buttonM3, buttonM4, buttonM6;
+    Button buttonM1, buttonM3, buttonM4 ;
 
     protected void $define() {
         Main = new VerticalArrangement(this);
@@ -52,12 +52,12 @@ public class Main_Menu extends Form implements HandlesEventDispatching{
         HorizontalArrangementM8.Height(20);
         HorizontalArrangementM1 = new HorizontalArrangement(Main);
         buttonM1 = new Button(HorizontalArrangementM1);
-        buttonM1.Text("Configure WiFi Connection");
+        buttonM1.Text("Configuration");
         buttonM1.Height(60);
         buttonM1.FontSize(20);
         buttonM1.TextAlignment(Component.ALIGNMENT_CENTER);
-        HorizontalArrangementM5 = new HorizontalArrangement(Main);
-        HorizontalArrangementM5.Height(20);
+       // HorizontalArrangementM5 = new HorizontalArrangement(Main);
+       // HorizontalArrangementM5.Height(20);
         HorizontalArrangementM3 = new HorizontalArrangement(Main);
         buttonM3 = new Button(HorizontalArrangementM3);
         buttonM3.Text("View CO2 Details");
@@ -75,14 +75,14 @@ public class Main_Menu extends Form implements HandlesEventDispatching{
         HorizontalArrangementM7 = new HorizontalArrangement(Main);
         HorizontalArrangementM7.Height(20);
         HorizontalArrangementM10 = new HorizontalArrangement(Main);
-        buttonM6 = new Button(HorizontalArrangementM10);
-        buttonM6.Text("Settings");
-        buttonM6.Height(60);
-        buttonM6.FontSize(20);
-        buttonM6.TextAlignment(Component.ALIGNMENT_CENTER);
-        buttonM5 = new Button(Main);
-        buttonM5.Text("Back");
-        buttonM5.FontSize(14);
+       // buttonM6 = new Button(HorizontalArrangementM10);
+       // buttonM6.Text("Settings");
+        //buttonM6.Height(60);
+       // buttonM6.FontSize(20);
+       // buttonM6.TextAlignment(Component.ALIGNMENT_CENTER);
+       //buttonM5 = new Button(Main);
+       // buttonM5.Text("Back");
+       // buttonM5.FontSize(14);
         HorizontalArrangementM9 = new HorizontalArrangement(Main);
         HorizontalArrangementM9.HeightPercent(100);
         EventDispatcher.registerEventForDelegation(this, formName, "BackPressed");
@@ -100,6 +100,7 @@ public class Main_Menu extends Form implements HandlesEventDispatching{
 
         dbg("dispatchEvent: " + formName + " [" + component.toString() + "] [" + componentName + "] " + eventName);
         if (eventName.equals("BackPressed")) {
+            this.finish();
             return true;
         } else if (eventName.equals("Click")) {
             if (component.equals(buttonM1)) {
@@ -112,13 +113,13 @@ public class Main_Menu extends Form implements HandlesEventDispatching{
             } else if (component.equals(buttonM4)) {
                 switchFormWithStartValue("Temperature", null);
                 return true;
-            } else if (component.equals(buttonM5)) {
-                switchFormWithStartValue("Home", null);
-                return true;
-            } else if (component.equals(buttonM6)) {
-                switchFormWithStartValue("GeneralApplicationSettings", null);
-                return true;
-            }
+            } //else if (component.equals(buttonM5)) {
+               // switchFormWithStartValue("Home", null);
+               // return true;
+             //else if (component.equals(buttonM6)) {
+               // switchFormWithStartValue("GeneralApplicationSettings", null);
+               // return true;
+           // }
         }
         return false;
     }
